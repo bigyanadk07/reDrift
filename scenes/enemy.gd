6fb3c86b0452e2af7bce288e6c1cb73ebfda419d
bigyanadk07 <bigyanadk07@gmail.com>
2024-09-03 21:17:13 +0545

    added boss1 mob

---
+++ scenes/enemy.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+var speed = 50
+var player_chase = false
+var player = null
+
+func _ready():
+	$AnimatedSprite2D.play("idle")
+
+func _physics_process(delta):
+	if player_chase: 
+		position += (player.position - position)/speed
+		$AnimatedSprite2D.play("move ")
+		
+
+
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
