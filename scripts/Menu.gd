791d914c261b9311b44584909f41ccc3ba31f782
bigyanadk07 <bigyanadk07@gmail.com>
2024-09-06 14:51:03 +0545

    Added Menu at the start

---
+++ scripts/Menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+
+func _on_multiplayer_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://scenes/option_menu.tscn")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
---
+++ scripts/option_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+
+
+
+func _on_volume_pressed():
+	pass # Replace with function body.
+
+
+func _on_difficulty_pressed():
+	pass # Replace with function body.
+
+
+func _on_keybinds_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
