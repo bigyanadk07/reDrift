1a59a23267c5b013f45f2fce6ab95629b0ba23f5
Bigyan Adhikari <bigyanadk07@gmail.com>
2024-09-17 14:36:00 +0545

    Added Butotn click sounds in the Main Menu

--- scenes/scripts/Menu.gd
+++ scenes/scripts/Menu.gd
@@ -3,20 +3,31 @@ extends Control
 
 
 func _on_play_pressed():
+	$ButtonClickSound.play()
+	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 
 func _on_multiplayer_pressed():
-	pass # Replace with function body.
+	$ButtonClickSound.play()
+	await get_tree().create_timer(0.2).timeout
 
 
 func _on_options_pressed():
+	$ButtonClickSound.play()
+	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://scenes/option_menu.tscn")
+	
 
 
 func _on_exit_pressed():
+	$ButtonClickSound.play()
+	await get_tree().create_timer(0.2).timeout
 	get_tree().quit()
+	
 
 
 func _on_how_to_pressed():
+	$ButtonClickSound.play()
+	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://scenes/Howto.tscn")
